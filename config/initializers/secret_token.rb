65f18367ec41e1a91ec59b9dfd123c7598a5e594
julio <julio336@hotmail.com>
2013-03-19 22:44:58 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BesameMucho::Application.config.secret_token = '36805cefc7c50088a7321b45e0d30ffa77d35ee4e0c330e2d6225b706926de0467e70b507decc5f1cce92ded88f26f0f6d540996f7b644e0ca42e6c338503bf5'

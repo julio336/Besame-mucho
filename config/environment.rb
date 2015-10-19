776f0d629a0210b680b0d952bcd811473756aeaf
julio <julio336@hotmail.com>
2015-10-19 18:27:43 -0500

    rack

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BesameMucho::Application.initialize!
+
+config.middleware.use Rack::Cache,
+   :verbose => true,
+   :metastore   => 'file:/var/cache/rack/meta',
+   :entitystore => 'file:/var/cache/rack/body'
